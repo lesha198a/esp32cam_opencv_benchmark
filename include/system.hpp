c03784063ece178ae5740cb279ff1c2fcd3b3813
Oleksii Kalnytskyi <oleksii.kalnytskyi@gmail.com>
2025-02-27 13:24:59 +0200

    Add support for esp32cam and platformio

---
+++ include/system.hpp
@@ -0,0 +1,23 @@
+//
+// Created by joachim on 24.03.20.
+//
+
+#ifndef SYSTEM_HPP
+#define SYSTEM_HPP
+
+// #include <sys/unistd.h>
+// #include <esp_err.h>
+
+// Initialize the SPIFFS
+esp_err_t init_spiffs();
+
+// Display cpu and memory info
+void disp_mem_infos();
+
+// Holds the task for a given amount of msec
+void wait_msec(uint16_t v);
+
+// Holds the task for a full second
+void wait_sec(uint16_t v);
+
+#endif //SYSTEM_HPP
